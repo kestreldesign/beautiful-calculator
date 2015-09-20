f5ef64db8fb96804bb6fdef21d4c00a9e6d20abc
The Deblings <thedeblings@Deblings-iMac.home>
2015-09-20 18:53:04 +0100

    Changed buttons to transp with UIImageView beneath (aspect issues). Added label and custom font.

--- BeautifulCalculator/ViewController.swift
+++ BeautifulCalculator/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
