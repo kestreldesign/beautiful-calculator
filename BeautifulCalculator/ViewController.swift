5e1a96066738a2f36e23d2fd86e36fd6ef6ebbf3
The Deblings <thedeblings@Deblings-iMac.home>
2015-09-20 19:27:48 +0100

    added action to numerical buttons and decimal point, appending to leftInput var.

--- BeautifulCalculator/ViewController.swift
+++ BeautifulCalculator/ViewController.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
    
+    @IBOutlet weak var displayText: UILabel!
+    var leftInput = ""
 
+    @IBAction func numberButtonPressed(btn: UIButton) {
+        if btn.tag == -1 {
+            leftInput += "."
+        } else {
+            leftInput += "\(btn.tag)"
+        }
+        displayText.text = leftInput
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
