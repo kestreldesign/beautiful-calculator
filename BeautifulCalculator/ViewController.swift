bfd28db84dd696a2a9f619fe471204928ecf195b
The Deblings <thedeblings@Deblings-iMac.home>
2015-09-21 11:37:01 +0100

    added functions for operators, edited to remove .0

--- BeautifulCalculator/ViewController.swift
+++ BeautifulCalculator/ViewController.swift
@@ -10,18 +10,93 @@ import UIKit
 
 class ViewController: UIViewController {
    
+    //outlets
     @IBOutlet weak var displayText: UILabel!
-    var leftInput = ""
+    
+    //variables
+    var currentInput = ""
+    var leftInput = "0"
+    var operatorPressed: Operator = .Empty
+    
+    //enums
+    enum Operator {
+        case Add
+        case Divide
+        case Multiply
+        case Subtract
+        case Empty
+    }
+    
 
     @IBAction func numberButtonPressed(btn: UIButton) {
         if btn.tag == -1 {
-            leftInput += "."
+            currentInput += "."
         } else {
-            leftInput += "\(btn.tag)"
+            currentInput += "\(btn.tag)"
         }
-        displayText.text = leftInput
+        displayText.text = currentInput
     }
     
+    @IBAction func addButtonWasPressed(sender: UIButton) {
+        doCalculation()
+        operatorPressed = .Add
+        storeLeftInput()
+    }
+    @IBAction func divideButtonWasPressed(sender: UIButton){
+        doCalculation()
+        operatorPressed = .Divide
+        storeLeftInput()
+    }
+    @IBAction func multiplyButtonWasPressed(sender: UIButton){
+        doCalculation()
+        operatorPressed = .Multiply
+        storeLeftInput()
+    }
+    @IBAction func subtractButtonWasPressed(sender: UIButton){
+        doCalculation()
+        operatorPressed = .Subtract
+        storeLeftInput()
+    }
+    @IBAction func clearButtonWasPressed(sender: UIButton){
+        operatorPressed = .Empty
+        currentInput = ""
+        leftInput = "0"
+        displayText.text = "0"
+    }
+    @IBAction func equalsButtonWasPressed(sender: AnyObject) {
+        doCalculation()
+        operatorPressed = .Empty
+        storeLeftInput()
+    }
+    func doCalculation(){
+        var theResult = ""
+        if operatorPressed != Operator.Empty {
+            if currentInput != "" {
+            if operatorPressed == .Add {
+                theResult = "\(Double(leftInput)! + Double(currentInput)!)"
+            } else if operatorPressed == .Divide {
+                theResult = "\(Double(leftInput)! / Double(currentInput)!)"
+            } else if operatorPressed == .Multiply {
+                theResult = "\(Double(leftInput)! * Double(currentInput)!)"
+            } else if operatorPressed == .Subtract {
+                theResult = "\(Double(leftInput)! - Double(currentInput)!)"
+            }
+            if Double(theResult)! % 1 == 0 {
+                let substringRange = theResult.endIndex.advancedBy(-2)..<theResult.endIndex
+                theResult.removeRange(substringRange)
+            }
+            displayText.text = theResult
+            currentInput = theResult
+            }
+        }
+    }
+    
+    func storeLeftInput(){
+        if currentInput != ""{
+            leftInput = currentInput
+            currentInput = ""
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
